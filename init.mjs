45284b086c041ef4fdc31c592ff3ba2ebdf2c449
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-02-16 18:09:18 +0000

    Initial commit

---
+++ init.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env zx
+
+$.verbose = false
+const fs = require('fs')
+const os = require('os')
+const url = require('url')
+const __tmpdir = os.tmpdir()
+const __cwd = process.cwd()
+const __base_url = 'https://gitlab.dev.davenportiowa.com/-/snippets/1/raw/master'
+
+export async function downloadScript(script, baseUrl = __base_url) {
+    const scriptUrl = `${baseUrl}/${script}`
+    const response = await fetch(scriptUrl)
+    const arrayBuffer = await response.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    const outputPath = url.pathToFileURL(`${__tmpdir}/${script}`)
+    fs.writeFileSync(outputPath, buffer)
+    return outputPath
+}
+
+main()
+
+async function main() {
+    const thisScript = 'init.mjs'
+    await downloadScript(thisScript)
+
+    const nextScript = await downloadScript(`${argv.script}.mjs`)
+    await import(`${nextScript}?${argv.args}`)
+}
